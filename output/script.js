675ef8e466de42e9029cdf921cc6aa294a73e0c4
Nazmul1582 <nazmulhasan1582@gmail.com>
2022-12-31 18:49:49 +0600

    Generic - TypeScript- Lesson13

--- output/script.js
+++ output/script.js
@@ -1,23 +1,51 @@
-// Interfaces  - TypeScript - Lesson12
-import Player from "./classes/Player.js";
-const mashrafi = new Player("Mashrafi", 36, "Bangladesh");
-let sakib;
-sakib = new Player("Sakib", 35, "Bangladesh");
-const players = [];
-players.push(mashrafi, sakib);
-console.log(players);
-// interface rectangleOptions {
-//   length: number;
-//   width: number;
-// }
-// const drawRectangle = (options: rectangleOptions) => {
-//   let length = options.length;
-//   let width = options.width;
-//   console.log(length, width);
+"use strict";
+// Working with Generic - typescript - lesson13
+// before Generic
+// const addID = (obj: object) => {
+//   let id = Math.floor(Math.random() * 100);
+//   return { ...obj, id };
 // };
-// const threeDOption = {
-//   length: 30,
-//   width: 20,
-//   height: 10,
+// const user = addID({
+//   name: "Mashrafi",
+//   age: 35,
+// });
+// After Generic
+// const addID = <T>(obj: T) => {
+//   let id = Math.floor(Math.random() * 100);
+//   return { ...obj, id };
 // };
-// drawRectangle(threeDOption);
+// const user = addID({
+//   name: "Mashrafi",
+//   age: 35,
+// });
+// console.log(user.name);
+// but the problem is:
+// const addID = <T>(obj: T) => {
+//     let id = Math.floor(Math.random() * 100);
+//     return { ...obj, id };
+//   };
+//   const user = addID("Mashrafi");     // Now addID() takes also string and etc.
+// Generic => more specific
+// const addID = <T extends object>(obj: T) => {
+//   let id = Math.floor(Math.random() * 100);
+//   return { ...obj, id };
+// };
+// const user = addID("Mashrafi"); // Error: Argument of type 'string' is not assignable to parameter of type 'object'
+// const addID = <T extends object>(obj: T) => {
+//   let id = Math.floor(Math.random() * 100);
+//   return { ...obj, id };
+// };
+// const user = addID({
+//   name: "Mashrafi",
+//   age: 36,
+// });
+// console.log(user.name);
+const addID = (obj) => {
+    let id = Math.floor(Math.random() * 100);
+    return Object.assign(Object.assign({}, obj), { id });
+};
+const user = addID({
+    name: "Mashrafi",
+    age: 36,
+});
+console.log(user.age);
